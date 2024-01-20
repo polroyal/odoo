89dbfed021f3e92c9d57f191e0c4d40fb6471556
polroyal <polycarproyal@gmail.com>
2024-01-20 14:09:03 +0300

    db-addr

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ fi
 
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
-: ${HOST:=${DB_PORT_5432_TCP_ADDR:='3.75.158.163'}}
+: ${HOST:=${DB_PORT_5432_TCP_ADDR:='3.125.183.140'}}
 : ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
 : ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
 : ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
